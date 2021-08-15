0528d6b06506e4b42f4d1efe0da5d10081920261
lyj <lyj@dareway.com.cn>
2021-08-15 22:31:06 +0800

    MOD：构造者模式改造Msg实体类

--- im-common/src/main/java/com/bigyj/entity/Msg.java
+++ im-common/src/main/java/com/bigyj/entity/Msg.java
@@ -1,42 +1,59 @@
 package com.bigyj.entity;
 
+import lombok.Data;
+
+@Data
 public class Msg {
-    private MsgType msgType ;
-    private User user;
-    private boolean success ;
-    private String toUserId ;
-    public boolean isSuccess() {
-        return success;
+    /**
+     * 消息类型
+     */
+    private final MsgType msgType ;
+    /**
+     * 用户信息
+     */
+    private final User user;
+    /**
+     * 消息是否发送成功
+     */
+    private final boolean success ;
+    /**
+     * 消息接收用户id
+     */
+    private final String toUserId ;
+    /**
+     * 消息内容
+     */
+    private final String content ;
+    public Msg(Builder builder){
+        this.msgType = builder.msgType;
+        this.user = builder.user;
+        this.success = builder.success;
+        this.toUserId = builder.toUserId;
+        this.content = builder.content;
     }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
+    public static class Builder{
+        private MsgType msgType ;
+        private User user;
+        private boolean success ;
+        private String toUserId ;
+        private String content ;
+        public Builder(MsgType msgType,User user){
+            this.msgType = msgType;
+            this.user = user ;
+        }
+        public Builder setSuccess(boolean success){
+            this.success = success ;
+            return this;
+        }
+        public Builder setToUserId(String toUserId){
+            this.toUserId = toUserId ;
+            return this;
+        }
+        public Builder setContent(String content){
+            this.content = content ;
+            return this;
+        }
     }
-
-    public MsgType getMsgType() {
-        return msgType;
-    }
-
-    public void setMsgType(MsgType msgType) {
-        this.msgType = msgType;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getToUserId() {
-        return toUserId;
-    }
-
-    public void setToUserId(String toUserId) {
-        this.toUserId = toUserId;
-    }
-
     public enum MsgType
     {
         LOGIN_REQUEST,//登陆请求消息
@@ -45,6 +62,5 @@ public class Msg {
         LOGOUT_REQUEST,// 退出请求消息
         LOGOUT_RESPONSE,// 退出响应消息
         HEART_BEAT,//心跳
-
     }
 }
