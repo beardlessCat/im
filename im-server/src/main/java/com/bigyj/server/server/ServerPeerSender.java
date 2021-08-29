4e415ad5865c3cea0d11f57aa7e3aafffcfd711f
lyj <lyj@dareway.com.cn>
2021-08-29 21:05:27 +0800

    MOD:解决消息无法转发的问题

--- im-server/src/main/java/com/bigyj/server/server/ServerPeerSender.java
+++ im-server/src/main/java/com/bigyj/server/server/ServerPeerSender.java
@@ -1,6 +1,7 @@
 package com.bigyj.server.server;
 
 import com.bigyj.entity.ServerNode;
+import com.bigyj.server.handler.ChatRedirectHandler;
 import com.bigyj.server.handler.client.ServerBeatHandler;
 import com.bigyj.server.handler.client.ServerExceptionHandler;
 import com.bigyj.server.worker.ServerWorker;
@@ -42,10 +43,14 @@ public class ServerPeerSender {
 						ChannelPipeline pipeline = ch.pipeline();
 						pipeline.addLast("decoder",new StringDecoder(CharsetUtil.UTF_8));
 						pipeline.addLast("encoder",new StringEncoder(CharsetUtil.UTF_8));
+						//服务间消息转发
+						pipeline.addLast("serverChatRedirect",new ChatRedirectHandler());
 						//服务端之间的心跳
 						pipeline.addLast("serverBeat",new ServerBeatHandler());
 						//服务间的重连
 						pipeline.addLast("serverException",new ServerExceptionHandler());
+
+
 					}
 				});
 			Channel connectChannel = bootstrap.connect(serverNode.getHost(), serverNode.getPort()).sync().channel();
