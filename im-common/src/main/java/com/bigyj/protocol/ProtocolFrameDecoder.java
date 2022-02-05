ede428de51b56f25c96fbb26b73332b1cb317816
lyj <948228970@qq.com>
2022-02-05 10:09:10 +0800

    add:增加handler处理半包粘包问题

---
+++ im-common/src/main/java/com/bigyj/protocol/ProtocolFrameDecoder.java
@@ -0,0 +1,14 @@
+package com.bigyj.protocol;
+
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+public class ProtocolFrameDecoder extends LengthFieldBasedFrameDecoder {
+
+    public ProtocolFrameDecoder() {
+        this(1024, 12, 4, 0, 0);
+    }
+
+    public ProtocolFrameDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength, int lengthAdjustment, int initialBytesToStrip) {
+        super(maxFrameLength, lengthFieldOffset, lengthFieldLength, lengthAdjustment, initialBytesToStrip);
+    }
+}
