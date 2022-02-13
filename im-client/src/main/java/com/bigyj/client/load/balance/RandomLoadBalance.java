0e7dc923482f016c6068128cfe9b2b7913b443d8
lyj <948228970@qq.com>
2022-02-13 15:40:55 +0800

    mod:完善随机选择策略，增加权重

--- im-client/src/main/java/com/bigyj/client/load/balance/RandomLoadBalance.java
+++ im-client/src/main/java/com/bigyj/client/load/balance/RandomLoadBalance.java
@@ -12,8 +12,21 @@ public class RandomLoadBalance extends AbstractLoadBalance{
 	public static final String NAME = "random";
 	@Override
 	protected ServerNode doSelect(List<ServerNode> serverNodes) {
-		//权重是否一致 fixme
-		int randomIndex = ThreadLocalRandom.current().nextInt(serverNodes.size());
-		return serverNodes.get(randomIndex);
+		int size = serverNodes.size();
+		int[] weights = new int[size];
+		int total = 0 ;
+		for(int i=0;i<size;i++){
+			Integer weight = serverNodes.get(i).getWeight();
+			total+=weight;
+			weights[i] = total;
+		}
+		//类似于抽奖算法
+		int randomIndex = ThreadLocalRandom.current().nextInt(weights[size - 1]);
+		for(int index = 0;index<size;index++){
+			if(randomIndex < weights[index]){
+				return serverNodes.get(index);
+			}
+		}
+		return serverNodes.get(ThreadLocalRandom.current().nextInt(serverNodes.size()));
 	}
 }
